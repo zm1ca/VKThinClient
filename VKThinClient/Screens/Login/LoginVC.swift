e73510ffccd36898e0c832baaa292d740c19f8d1
zm1ca <zmicier.fied@yandex.ru>
2021-06-28 12:43:34 +0300

    add empty login screen

--- VKThinClient/Screens/Login/LoginVC.swift
+++ VKThinClient/Screens/Login/LoginVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginVC.swift
 //  VKThinClient
 //
 //  Created by Źmicier Fiedčanka on 28.06.21.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemTeal
     }
-
-
 }
-
