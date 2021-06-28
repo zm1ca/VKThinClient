97f18828519b543ec73ec7b9381dce2e9800db59
zm1ca <zmicier.fied@yandex.ru>
2021-06-28 12:37:31 +0300

    Initial Commit

---
+++ VKThinClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKThinClient
+//
+//  Created by Źmicier Fiedčanka on 28.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
