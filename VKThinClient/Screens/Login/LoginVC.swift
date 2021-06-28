2b604fefa71b457e956626792bbb52e9e1deeb1c
zm1ca <zmicier.fied@yandex.ru>
2021-06-28 15:39:57 +0300

    feat: vk logo as background for Launch and Login screens

--- VKThinClient/Screens/Login/LoginVC.swift
+++ VKThinClient/Screens/Login/LoginVC.swift
@@ -14,7 +14,6 @@ class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemTeal
         authService = SceneDelegate.shared().authService
     }
     
